a251ff6405e17a1d6a892362fe8611b4a9b2e726
Miss Bisht <73991231+Psych4-383@users.noreply.github.com>
2022-08-21 08:58:24 +0530

    Initial commit
    1. Begin with node app
    2. Basic folder structure

---
+++ index.js
@@ -0,0 +1,20 @@
+// imports
+const express = require('express')
+const ejs = require('ejs')
+
+// main variables
+const app = express()
+const port = process.env.PORT || 3000
+
+// set the view engine to ejs and other parameters
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
+
+// home page
+app.get('/', (req, res) => {
+    res.render('index')
+})
+
+app.listen(port, () => {
+    console.log(`Server started on port ${port}`)
+})
